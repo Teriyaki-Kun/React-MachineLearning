b97a5e4c7749e8ce0e1f5d0cc10451fefcbc78dc
Teriyaki-Kun <michhu1212@gmail.com>
2025-12-01 19:46:49 +0700

    ganti api

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const http = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://if31flask-python-production.up.railway.app/",
   headers: {
     Accept: "application/json",
   },
